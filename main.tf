2b3b3c539ada34f55dc1a81da35735ce4108cfbf
Dmytro <dima.shutko5@gmail.com>
2026-06-02 21:27:33 +0200

    deleted provider

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.75.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {
-
-  }
-}
-
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
